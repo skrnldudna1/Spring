d5511a5d32f730d56141699532e5cb89fd60479f
skrnldudna1 <skrnldudna1@naver.com>
2024-12-04 17:40:24 +0900

    수정

--- 회원관리프로그램 실습중(ing)/member_framework/src/main/webapp/WEB-INF/mysql/member_table.sql
+++
@@ -1,9 +0,0 @@
-drop table member_table;
-create table member_table(
-	id bigint primary key auto_increment,
-    memberEmail varchar(20) unique,
-    memberPassword varchar(20),
-    memberName varchar(20),
-    memberAge int,
-    memberMobile varchar(30)
-);
